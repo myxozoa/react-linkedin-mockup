bb1e53805ce95e4862379cfd94e7764c19c18bf4
Ronnie Miksch <fakepreferences@gmail.com>
2018-02-08 15:13:24 -0800

    Connections component now finished and styled

--- src/components/Connections.js
+++ src/components/Connections.js
@@ -13,6 +13,19 @@ class Connections extends React.Component {
                 <div className='your'> Your connections </div>
                 <div className='see-all'> See all </div>
                 <img src={bubbles} alt='borked' />
+                <div className='conn-break'></div>
+                <div className='contacts'>Add personal contacts</div>
+                <form>
+                    <input autoComplete='on' type='email' placeholder='   Email' />
+                    <button> Continue </button>
+                </form>
+                <div className='conn-options'> <div>More options</div> </div>
+                <div className='conn-info'>
+                    <div>
+                    <span className='conn-info-text'>We'll import your address book to suggest conections.</span>
+                    <span className='conn-info-link'> Learn more </span>
+                    </div>
+                </div>
             </div>
         );
     }
