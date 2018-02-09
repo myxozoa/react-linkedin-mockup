32f55324508e0ab005cbe0e3c10bbafcfb97e1c1
Ronnie Miksch <fakepreferences@gmail.com>
2018-02-08 16:38:53 -0800

    Added styled promo component, with placeholder ads

--- src/components/Promoted.js
+++ src/components/Promoted.js
@@ -1,16 +1,15 @@
 import React from 'react';
+import promo from '../assets/promotedsites.png';
 
-class Promoted extends React.Component {
-    state = {
-
-    }
-
-    render() {
-        return (
-            <div className='promoted'>
-
+function Promoted() {
+    return (
+        <div className='promoted'>
+            <div className='promo-title'>
+                <div> Promoted </div>
+                <div> &#x22ef; </div>
             </div>
-        );
-    }
+            <img src={promo} alt='borked' />
+        </div>
+    );
 }
 export default Promoted;
